737f322bac0a0a3b63293623eb4c8be1d2591f1d
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-13 21:16:42 +0500

    new section is start

--- components/Company.jsx
+++ components/Company.jsx
@@ -1,6 +1,8 @@
 "use client";
 import style from "@/styles/company.module.scss";
 import React from 'react'
+import Image from "next/image";
+import robots from "@/public/rename.jpeg"
 
 const Company = () => {
     return (
@@ -8,8 +10,9 @@ const Company = () => {
             <div className={style.btn}>
                 <button>Company</button>
             </div>
-
-            
+        <div className={style.robot}>
+        <Image src={robots} alt="" />
+        </div>
         </div>
     )
 }
