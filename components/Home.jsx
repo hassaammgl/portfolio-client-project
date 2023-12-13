0a2aef61b6c74f90f9135553bd02c6db80afa869
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-13 15:56:24 +0500

    internet te search

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import style from "@/styles/home.module.scss";
+import {FaHandPeace} from "react-icons/fa"
 
 const Home = () => {
   return (
-    <div>
-      <div className="left">
-        home
+    <div className={style.home}>
+      <div className={style.left}>
+        <div className={style.box}>
+        <FaHandPeace className={style.hand} />
+        Hi There! I'm Monster Programming
+        </div>
+        <div className={style.heading}>
+            <h1>A <span>Frontend Engineer.</span>I Help Startups <span>Launch</span>And <span>Grow</span> Their Products</h1>
+        </div>
       </div>
     </div>
   )
