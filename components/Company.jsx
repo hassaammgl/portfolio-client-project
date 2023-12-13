0154dc7f055f42dc352a2ec44a61991643a30398
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-13 21:07:53 +0500

    new image add

--- components/Company.jsx
+++ components/Company.jsx
@@ -3,13 +3,13 @@ import style from "@/styles/company.module.scss";
 import React from 'react'
 
 const Company = () => {
-  return (
-    <div className={style.company}>
-      <div className={style.btn}>
+    return (
+        <div className={style.company}>
+            <div className={style.btn}>
                 <button>Company</button>
             </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Company
