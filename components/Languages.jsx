83b2fea27c4ab4d252d13817ea837baf8e8d7a5f
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-13 20:16:35 +0500

    dfasf

--- components/Languages.jsx
+++ components/Languages.jsx
@@ -1,62 +1,66 @@
-import React from 'react'
-import { FaReact , FaSass } from "react-icons/fa";
+import React from "react";
+import { FaReact, FaSass } from "react-icons/fa";
 import { TbPolygon } from "react-icons/tb";
-import { SiSolidity ,SiTypescript } from "react-icons/si";
-import { IoLogoFigma , IoLogoJavascript } from "react-icons/io5";
+import { SiSolidity, SiTypescript } from "react-icons/si";
+import { IoLogoFigma, IoLogoJavascript } from "react-icons/io5";
 import { DiMongodb } from "react-icons/di";
-import { FaNodeJs ,FaAngular } from "react-icons/fa";
+import { FaNodeJs, FaAngular } from "react-icons/fa";
 import style from "@/styles/language.module.scss";
 
 const Languages = () => {
-  return (
-    <div className={style.technology}>
-        <div className={style.btn}>
-        <button>TECHNOLOGIES</button>
+    return (
+        <div className={style.technology}>
+            <div className={style.btn}>
+                <button>TECHNOLOGIES</button>
+            </div>
+            <div className={style.tech}>
+                <div className={style.tec}>
+                    <div className={style.techno}>
+                        <FaReact className={style.icons} />
+                        React
+                    </div>
+                    <div className={style.techno}>
+                        <FaAngular className={style.icons} />
+                        Angular
+                    </div>
+                    <div className={style.techno}>
+                        <FaNodeJs className={style.icons} />
+                        Node
+                    </div>
+                    <div className={style.techno}>
+                        <DiMongodb className={style.icons} />
+                        MongoDB
+                    </div>
+                    <div className={style.techno}>
+                        <IoLogoJavascript className={style.icons} />
+                        Javascript
+                    </div>
+                </div>
+                <div className={style.tec}>
+                    <div className={style.techno}>
+                        <IoLogoFigma className={style.icons} />
+                        Figma
+                    </div>
+                    <div className={style.techno}>
+                        <FaSass className={style.icons} />
+                        Sass
+                    </div>
+                    <div className={style.techno}>
+                        <SiTypescript className={style.icons} />
+                        Typescript
+                    </div>
+                    <div className={style.techno}>
+                        <SiSolidity className={style.icons} />
+                        Solidity
+                    </div>
+                    <div className={style.techno}>
+                        <TbPolygon className={style.icons} />
+                        Polygon
+                    </div>
+                </div>
+            </div>
         </div>
-      <div className={style.tech}>
-        <div className={style.techno}>
-            <FaReact />
-            React
-        </div>
-        <div className={style.techno}>
-            <FaAngular />
-            Angular
-        </div>
-        <div className={style.techno}>
-            <FaNodeJs />
-            Node
-        </div>
-        <div className={style.techno}>
-            <DiMongodb />
-            MongoDB
-        </div>
-        <div className={style.techno}>
-            <IoLogoJavascript />
-            Javascript
-        </div>
-        <div className={style.techno}>
-            <IoLogoFigma />
-            Figma
-        </div>
-        <div className={style.techno}>
-            <FaSass />
-            Sass
-        </div>
-        <div className={style.techno}>
-            <SiTypescript />
-            Typescript
-        </div>
-        <div className={style.techno}>
-            <SiSolidity />
-            Solidity
-        </div>
-        <div className={style.techno}>
-            <TbPolygon />
-            Polygon
-        </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Languages
+export default Languages;
